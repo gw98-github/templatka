fbdeb44352d5a0380471857a5ede3a2956adfe4f
Grzegorz Ważny <61500499+gw98-github@users.noreply.github.com>
2023-08-25 05:35:54 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+@font-face {
+  font-family: 'Roboto';
+  src:
+    local('Roboto'),
+    url(./fonts/Roboto/Roboto-Regular.ttf) format('truetype');
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import AboutPage from '@/components/AboutPage.vue'
+</script>
+<template>
+  <div>
+    <AboutPage />
+  </div>
+</template>
+
+<style></style>
